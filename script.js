9314331b0b20cedc1f74fa49007e43ce04456970
Tetiana Panasiuk <tetianapanasiuk93@gmail.com>
2018-10-16 13:21:32 +0200

    Active link style added

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+let slides = document.querySelectorAll('#slides .slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide() {
     slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'slide showing';
+}
+
+// Get the container element
+let navContainer = document.getElementById("nav");
+
+// Get all buttons with class="btn" inside the container
+let navs = navContainer.getElementsByClassName("link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < navs.length; i++) {
+    navs[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+    });
 }
